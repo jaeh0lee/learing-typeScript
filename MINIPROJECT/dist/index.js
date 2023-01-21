9900eb651616332fe6e2b9af4a9e0d36296b3149
STRONGSHARK <woghfghf@naver.com>
2023-01-21 18:20:52 +0900

    미니프로젝트 레이아웃 작성

---
+++ MINIPROJECT/dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+console.log("I AM HUNGRY");
+console.log("woooo");
+function printDouble(msg) {
+    console.log(msg);
+    console.log(msg);
+}
+printDouble("helloe");
+printDouble("fuck");
---
+++ MINIPROJECT/src/index.ts
@@ -0,0 +1,9 @@
+console.log("I AM HUNGRY");
+console.log("woooo");
+
+function printDouble(msg: string) {
+  console.log(msg);
+  console.log(msg);
+}
+printDouble("helloe");
+printDouble("fuck");
