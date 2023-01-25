ee4e9a60530a5832358e0e028c5ad5ef200460f0
STRONGSHARK <woghfghf@naver.com>
2023-01-25 16:12:14 +0900

    간단한 todolist를 만들면 타입스크립트로 Dom 조작하는법 학습

--- MINIPROJECT/dist/index.js
+++ MINIPROJECT/dist/index.js
@@ -1,9 +1,41 @@
 "use strict";
-console.log("I AM HUNGRY");
-console.log("woooo");
-function printDouble(msg) {
-    console.log(msg);
-    console.log(msg);
-}
-printDouble("helloe");
-printDouble("fuck");
+/*
+타입스크립트는 문서 객체, 즉 DOM 요소 및 기타 요소의 타입을 인지합니다.
+ */
+/*
+타입스크립트 접근 방법에 non-null 단언 연산자가 있다. !(느낌표이다.)
+코드 줄 뒤에 느낌표를 붙임으로써 절대 null이 되지 않는다고 타입스크팁트에 약속하는것이다.
+하지만 non-null 방식은 값이 확실하게 존재하고 null이 아닐때만 사용한다.
+그런 이유로 불확실한 런타임에서 작업할때는 방법을 사용하지 않는다.
+*/
+// const button = document.getElementById("btn")!;
+// button?.addEventListener("click", () => {
+//   alert("click");
+// });
+/*
+ 타입 단언
+ 만약 타입스크립트에 내가 더 많이 알고 있고 분명히 문자열 타입이라고 단연하려면 타입 단언 구문을 사용하면 됩니다.
+ as 키워드를 타입 앞에 사용하면 된다.
+
+ */
+// let mystery: unknown = 4;
+// const numChars = (mystery as string).length;
+/*
+타입 단언 2
+타입스크립트에 inputdl 제네릭 HTML 요소가 아니라 HTML input 요소라는것을 타입 단언을 사용해 말했다.
+*/
+// const input = document.getElementById("todoinput")! as HTMLInputElement;
+// const btn = document.getElementById("btn")! as HTMLButtonElement;
+// btn.addEventListener("click", () => {
+//   alert(input.value);
+//   input.value = "";
+// });
+/*
+타입 단어의 대체 구문
+as 부분을 지우고 홀화살괄호(<>)를 사용해서 타입을 엑세스하거나 추가하고 단언하려는 타입의 변수를 뒤에 입력합니다.
+앞부분의 타입을 지우면 변수의 제네릭 HTML 요소로 추론됩니다.
+해당 구문은 덜 일반적이고 jsx에 지원하지 않으며 일반적으로는 해당 타입을 잘 사용하지는 않습니다.
+*/
+// const input = document.getElementById("todoinput")!;
+// const btn = document.getElementById("btn")! as HTMLButtonElement;
+// <HTMLInputElement>input;
---
+++ MINIPROJECT/dist/todolist.js
@@ -0,0 +1,59 @@
+"use strict";
+/*
+  todolist를 만들면서 익숙해지기
+  로컬스토리지란 브라우저 기반 스토리지이다. [텍스트나 문자열만을 저장할수 있는 배열  ]
+ */
+const btn = document.getElementById("btn");
+const input = document.getElementById("todoinput");
+const form = document.getElementById("todoform");
+const list = document.getElementById("todolist");
+// 배열에 Todo 타입 지정
+const todos = readTodos();
+// 로컬스토리지에 저장되있는 값을 초기페이지에 불러오기
+todos.forEach(createTodo);
+// 로컬스토리지에 저장되어 있는 값을 가져오기
+function readTodos() {
+    const todosJson = localStorage.getItem("todos");
+    if (todosJson === null)
+        return [];
+    return JSON.parse(todosJson);
+}
+//check박스를 클릭하면 로컬스토리지에 리스트 저장하기
+function saveTodos() {
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
+/*
+ 함수로 만들기
+ 함수로 만든뒤에 파라미터에 event를 입력하면 암묵적으로 any타입이라고 타입스크립트가 생각을 합니다.
+ 타입스크립트에 어떻게 함수가 호출되고 e가 무엇인지를 알려주어야 합니다.
+ 그러기 위해서 SubmitEvent라는 타입을 e에게 알려줍니다
+ 정리하자면 함수 정의가 분리되고 참조만 전달하면 타입스크립트가 추론을 못하기때문에 SubmitEvent 타입의 변수라고 해줘야 타입 추론이 가능한것입니다.
+*/
+// 이벤트리스너 함수
+function hanldeSubmit(e) {
+    e.preventDefault();
+    const newTodo = {
+        text: input.value,
+        completed: false,
+    };
+    // 로컬스토리지에 값 추가
+    createTodo(newTodo);
+    todos.push(newTodo);
+    saveTodos();
+    input.value = "";
+}
+// 리스트 함수 따로 생성
+function createTodo(todo) {
+    const newLI = document.createElement("li");
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.checked = todo.completed;
+    checkbox.addEventListener("change", () => {
+        todo.completed = checkbox.checked;
+        saveTodos();
+    });
+    newLI.append(todo.text);
+    newLI.append(checkbox);
+    list.append(newLI);
+}
+form.addEventListener("submit", hanldeSubmit);
